633962ee5c49ee4c0dc83dcd81ccd0508a5de9c4
Nathan Mayo <40875260+ntm52@users.noreply.github.com>
2021-06-25 09:31:11 -0400

    Add files via upload

---
+++ ThermometerServerandClient/Client/ClientCcode/Client.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <strings.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#define PORT 8080
+#define MAXLINE 1024
+
+int main(void) {
+  
+  int sockfd; 
+  char buffer[MAXLINE]; 
+  char tempType = ' '; 
+
+  struct sockaddr_in servaddr; 
+  struct hostent *server; 
+
+  FILE *readFile = fopen("desiredTemp.txt", 'r'); 
+  FILE *writeFile = fopen("receivedTemp.txt", 'w'); 
+
+  while (1 == 1) { 
+    
+    tempType = fgetc(readFile); 
+    bzero((char *)&servaddr, sizeof(servaddr)); 
+    
+    if((server = gethostbyname("LAPTOP-KTCF7DB5\0")) == NULL) {
+      
+      printf("Error, host name not valid\n"); 
+      exit(EXIT_FAILURE);
+      
+    }
+
+    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    //memset(&servaddr, 0, sizeof(servaddr));
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(PORT);
+    bcopy((char *)server->h_addr_list[0], (char *)&servaddr.sin_addr.s_addr, server->h_length);
+
+    int n, len;
+
+    sendto(sockfd, &tempType, 1,
+        0, (const struct sockaddr *) &servaddr,
+            sizeof(servaddr));
+            
+    printf("Message sent.\n");
+
+    n = recvfrom(sockfd, (char *)buffer, MAXLINE,
+                MSG_WAITALL, (struct sockaddr *) &servaddr,
+                &len);
+    buffer[n] = '\0';
+    printf("Server : %s\n", buffer);
+
+    close(sockfd);
+    // receive number
+    
+    // write file
+    fprintf(writeFile, "%s", buffer);
+
+  }
+
+}
---
+++ ThermometerServerandClient/Server/Server.ino
@@ -0,0 +1,37 @@
+int tempPin = 0;
+int tempDesired = 0;
+
+void setup() {
+
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  digitalWrite(1, LOW);
+  digitalWrite(2, HIGH);
+  if (Serial.available() > 0) {
+    tempDesired = Serial.read();
+
+    Serial.println(tempDesired, DEC);
+
+    int tempReading = analogRead(tempPin);
+
+    double tempK = log(10000.0 * ((1024.0 / tempReading - 1)));
+    tempK = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * tempK * tempK)) * tempK);
+
+    float tempCTemporary = tempK - 273.15;
+    int tempF = (tempCTemporary * 9.0) / 5.0 + 32.0;
+    int tempC = tempCTemporary * 1;
+    // Temperatures received, do something with them
+
+    if (tempDesired == 0) {
+      Serial.write(tempF);
+    } else {
+      Serial.write(tempC);
+    }
+    digitalWrite(1, HIGH);
+    digitalWrite(2, LOW);
+  }
+
+}
